ee6497747774e469429b60c941698ab9f10b18e9
Jorge <info@jorgebenitezlopez.com>
2024-11-05 12:31:02 +0100

    More time

--- api.test.js
+++ api.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const baseUrl = 'https://cristinamaser.com/';
 
 describe('GET api/invitations', () => {
+    jest.setTimeout(10000); 
     // it() es una función de alias que se utiliza comúnmente en el estilo de prueba BDD (Behavior-Driven Development) para describir el comportamiento esperado de una función o componente.
     it('responds with json', async () => {
         const responde = await request(baseUrl).get('api/invitations');
